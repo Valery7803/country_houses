3dcee2cd5804a18ff3f9278187ccf03c77d1ff25
Valery <125111692+Valery7803@users.noreply.github.com>
2024-08-06 12:16:14 +0300

    Создал шаблоны и навигацию по сайту

--- houses/village_houses/views.py
+++ houses/village_houses/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+menu = [
+    {'title':'Страница про нас', 'url_name':'about'},
+    {'title':'Наши контакты', 'url_name':'contacts'},
+]
 
 # Функция главной страницы
 def index(request):
-    return render(request,'village_houses/index.html')
+    data = {'title':'Главная страница','menu': menu}
+    return render(request,'village_houses/index.html', data)
 
 
 # Функция страницы про нас
 def about(request):
-    return render(request, 'village_houses/about.html')
+    data = {'title':'Страница про нас', 'menu': menu}
+    return render(request, 'village_houses/about.html', data)
 
 
 # Функция страницы контакты
 def contacts(request):
-    return render(request,'village_houses/contacts.html')
+    data = {'title':'Наши контакты', 'menu': menu}
+    return render(request,'village_houses/contacts.html', data)
 
 
 # Функция обзора выбранного дома
 def show_house(request, home_id):
-    return HttpResponse('Ваш дом {home_id}')
+    return HttpResponse(f'Ваш дом <p>id:{home_id}</p>')
